d54b115a33aae539b12e16a45d9170b918315d4f
alexissan702 <alexissan702@gmail.com>
2020-07-16 15:53:34 -0700

    front Page

---
+++ index.js
@@ -0,0 +1,10 @@
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var formToSubmit = document.getElementById("form");
+
+formToSubmit.addEventListener("submit", run);
+
+function run() {
+    
+}
+
