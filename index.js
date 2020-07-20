e9dc52f658bf37a6710a56a4c1d78d913ff858b3
alexissan702 <alexissan702@gmail.com>
2020-07-20 16:37:16 -0700

    added files

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-var username = document.getElementById("username");
+//var username = document.getElementById("username");
 var password = document.getElementById("password");
 var formToSubmit = document.getElementById("form");
 
+// username function
+var username = document.getElementById("username").value;
+
+
+
 formToSubmit.addEventListener("submit", run);
 
-function run() {
-    
+function run(event) {
+    console.log(username)
+    var login = [];
+    login.push({user: username.value, pass: password.value});
+    console.log(login);
+    event.preventDefault();
 }
 
+
+//Password 
+const successDiv = document.getElementById('PasswordSuccess');
+
+
+const passwordInput = document.getElementById('password');
+passwordInput.addEventListener('keyup',passwordCheck);
+
+var password = ''
+function passwordCheck(event){
+    password=password+event.key
+    console.log('thisIsThePassword',password);
+    if(password.length >= 8){
+        successDiv.classList.add('show');
+
+
+    }
+}
