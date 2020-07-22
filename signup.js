f18e17943032c8a70ec2afd4bbc8ceb9c4f83ef6
alexissan702 <alexissan702@gmail.com>
2020-07-22 16:55:36 -0700

    updated files

--- signup.js
+++ signup.js
@@ -1,18 +1,49 @@
 const userInput = document.getElementById("usernameInput");
+const passInput = document.getElementById("passwordInput");
+const conPassInput = document.getElementById("conPassInput");
 let securityQ = document.getElementById("securityquestion");
 let formToSubmit = document.getElementById("form");
+var login = []
 
-const successUser = document.getElementById('usernameSuccess');
+// count characters in username
+const successUser = document.getElementById('usernameSucess');
 userInput.addEventListener('keyup',userCheck);
 var username = ''
 function userCheck(event){
     username=username+event.key
     console.log('thisIsTheUsername',username);
-    if(username.length >= 8 && username.length <= 20){
-        successUser.classList.add("show");
+    if(username.length >= 8 ) {
+        successUser.classList.add("green");
+        successUser.classList.remove("red");
     }
 }
-
+//count characters in password
+const successPass = document.getElementById('passwordSucess');
+passInput.addEventListener('keyup',passCheck);
+var password = ''
+function passCheck(event){
+    password=password+event.key
+    console.log('thisIsTheUsername',password);
+    if(password.length >= 8 ) {
+        successPass.classList.add("green");
+        successPass.classList.remove("red");
+        //alert("pass");
+    }
+}
+//Password confirmed
+const confirmedPass = document.getElementById('passwordConfirmed');
+conPassInput.addEventListener('keyup',passCheck);
+var comPass = ''
+function conPassCheck(event){
+    conPass=conPass+event.key
+    console.log('thisIsTheConfrimedPassword',conPass);
+    if(conPass.value == conPass.value ) {
+        confirmedPass.classList.add("green");
+        confirmedPass.classList.remove("red");
+        //alert("pass");
+    }
+}
+//Event listener for submition 
 formToSubmit.addEventListener("submit", scan);
 
 function scan(event) {
@@ -20,6 +51,7 @@ function scan(event) {
     console.log(securityQ.value);
 }
 
+//function to register user
 /*function register() {
     var login = [];
     login.push({Firstname: firstname.value, userInput: userInput.value, pass: password.value});
