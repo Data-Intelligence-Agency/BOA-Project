e8d9fdf7b6a3504f924af4ec3fa55dd257ae9d27
alexissan702 <alexissan702@gmail.com>
2020-07-28 17:15:26 -0700

    upgraded files

--- signup.js
+++ signup.js
@@ -7,16 +7,31 @@ let formToSubmit = document.getElementById("form");
 var login = []
 
 // count characters in username
-const successUser = document.getElementById('usernameSucess');
-userInput.addEventListener('keyup', userCheck);
-var username = ''
-function userCheck(event) {
-    username = username + event.key
-    console.log('thisIsTheUsername', username);
-    if (username.length >= 8) {
-        successUser.classList.add("green");
-        successUser.classList.remove("red");
-    }
+//const successUser = document.getElementById('usernameSucess');
+//userInput.addEventListener('keyup', userCheck);
+//var username = ''
+//function userCheck(event) {
+   // username = username + event.key
+    //console.log('thisIsTheUsername', username);
+    //if (username.length >= 8) {
+        //successUser.classList.add("green");
+       // successUser.classList.remove("red");
+ //   }
+//}
+function validation(){
+    var user = document.getElementById("usernameInput");
+         if(user.value.length <= 20 && user.value.length >= 3){
+         }
+         else{
+             alert("Username has to be between 3-20 characters.")
+          }
+         //duplication data list
+         var user = document.getElementById("usernameInput");
+         if(user.value == list.value){
+         }
+         else{
+             alert("Username already exists.")
+          }
 }
 //count characters in password
 const charactersCount = document.getElementById("character");
@@ -30,9 +45,8 @@ const passwordCheck = document.getElementById("passwordSucess");
 function passCheck() {
     const password = passInput.value;
     console.log(password)
-    // console.log(event);
     passwordCheck.style.display = "block";
-    // console.log('thisIsThePassword', password);
+    //Character Count
     if (password.length >= 8 && password.length <= 20) {
         charactersCount.classList.add("green");
         charactersCount.classList.remove("red");
@@ -43,6 +57,7 @@ function passCheck() {
         charactersCount.classList.remove("green");
         return false;
     }
+    //Upper, Lowercased letters, & numbers
     const charregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/;
     let m;
     if ((m = charregex.exec(password)) !== null) {
@@ -51,52 +66,66 @@ function passCheck() {
             requiredCharacters.classList.remove("red");
         });
     }
-    const charSpecial = /^(?=.*[\W+]).*$/;
+    //No repeating characters 3 times or more
+    const repeating = /(.)\1{2,}/;
+    let o;
+    if ((o = repeating.exec(password)) !== null) {
+        o.forEach((match, groupIndex) => {
+            noRepeat.classList.remove("green");
+            noRepeat.classList.add("red");
+        });
+    }
+    //No Spaces and Special Characters
+    const charSpecial = /[@#()+{}\/?~;,*._-]/sg;
     let n;
     if ((n = charSpecial.exec(password)) !== null) {
         n.forEach((match, groupIndex) => {
-            requiredCharacters.classList.add("green");
-            requiredCharacters.classList.remove("red");
+            specialChar.classList.add("green");
+            specialChar.classList.remove("red");
         });
     }
-    else if (requiredCharacters.classList.contains("red") || charactersCount.classList.contains("red") || requiredCharacters.classList.contains("red")) {
-        return false;
+    const noSpace = /\s/; 
+    let p;
+    if ((p = noSpace.exec(password)) !== null) {
+        p.forEach((match, groupIndex) => {
+            specialChar.classList.remove("green");
+            specialChar.classList.add("red");
+        });
     }
-
 }
 //Password confirmed
 const confirmedPass = document.getElementById('passwordConfirmed');
-conPassInput.addEventListener('keyup', passCheck);
-var comPass = ''
+conPassInput.addEventListener('keyup', conPassCheck);
 function conPassCheck(event) {
-    conPass = conPass + event.key
-    console.log('thisIsTheConfrimedPassword', conPass);
-    if (conPass.value == conPass.value) {
+    console.log(conPassInput.value);
+    if (passInput.value == conPassInput.value) {
         confirmedPass.classList.add("green");
         confirmedPass.classList.remove("red");
-        //alert("pass");
+    }  
+}
+/*
+    //  dOcument.getElementById('answer');
+     const securityAnswer = document.getElementById('answer');
+     if (!testInput(true,"Secret Question Answer (1st entry)",
+            secretQuestionAnswerTF0.value.trim())){ 
+        return true; 
     }
-
+    // Input.addEventListener(click ,function);
     //  security question 
 
     // openmodal
-    $('.dropdown-trigger').each(function () {
-        $(this).click(function () {
-            $('body').addclass('modal-active')
 
-        });
+    //for each drop down option
+    // $('.dropdown-trigger').each(function () {
+    //     //if clicked on an option
+    //     $(this).click(function () {
+    //         //add class modal - active
+    //         // $('body').addclass('modal-active')
+    //     // console.log(this)
+    //     });
 
-
-    });
-
-    //close modal 
-    $('.model-close').cli
-
-
-
-
-
-}
+       
+    // });
 
 //Event listener for submition 
 formToSubmit.addEventListener("submit", scan);
